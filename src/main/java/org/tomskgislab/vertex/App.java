86984c6c62410588f9a208529955915f9d3c2bfc
filippov70 <filippov70@gmail.com>
2013-04-23 20:36:04 +0700

    Логгирование загрузки приложения - попытка выяснить, почему не запускается из jar

--- src/main/java/org/tomskgislab/vertex/App.java
+++ src/main/java/org/tomskgislab/vertex/App.java
@@ -2,27 +2,30 @@ package org.tomskgislab.vertex;
 
 import java.io.IOException;
 
-
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	private static final Logger logger = LogManager.getLogger( App.class );
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(App.class.getResource("Main.fxml"));
+			logger.info("loading fxml...");
 			BorderPane dh = (BorderPane) loader.load();
+			logger.info("loading fxml complit");
 			Scene s = new Scene(dh);
 			primaryStage.setScene(s);
 			primaryStage.show();
+			logger.info("Application started");
 		} 
-		catch (IOException e) {
-			e.printStackTrace();
+		catch (IOException ex) {
+			logger.error(ex.getMessage());
 			System.exit(-1);
 		}
 	}
